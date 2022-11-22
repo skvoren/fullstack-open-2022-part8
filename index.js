6e5557d54ce0c5b3ff7b737d945d17757a9848b6
skvoren <zayonvx@gmail.com>
2022-11-23 03:28:19 +0400

    exercise 8.4

--- index.js
+++ index.js
@@ -107,7 +107,7 @@ const typeDefs = gql`
   type Query {
     bookCount: Int!
     authorCount: Int!
-    allBooks: [Book!]!
+    allBooks(author: String!): [Book!]!
     allAuthors: [Author!]!
   }
 `
@@ -116,7 +116,7 @@ const resolvers = {
     Query: {
         bookCount: () => books.length,
         authorCount: () => authors.length,
-        allBooks: () => books,
+        allBooks: (root, args) => books.filter((b) => b.author === args.author),
         allAuthors: () => authors
     },
     Author: {
